93b0dbf29b7bb467c3190922fc14bcca9616bc41
Deeempho <tlhonolofatso44@gmail.com>
2024-09-24 21:00:11 +0100

    Improved UX

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 function displayRecipe(response) {
-  console.log("Recipe generated");
-
   new Typewriter("#recipe", {
     strings: response.data.answer,
     autoStart: true,
@@ -16,12 +14,13 @@ function generateRecipe(event) {
   let apiKey = "21dbt88fo9e3fd05aafa4a622bab45f2";
   let prompt = `User instructions are: Generate a recipe on ${instructionsInput.value}`;
   let context =
-    "You are a great cook and an expert when it comes to food. Your mission is to generate a simple recipe in basic HTML. Make sure to follow user instructions";
+    "You are a great cook and an expert when it comes to food. Your mission is to generate a simple and easy to read recipe. The poem must be in basic HTML format. Separate lines with a <br />. Make sure to follow user instructions. Sign the poem with 'SheCodes AI' inside a <strong> at the end of the poem and NOT at the beginning of the poem";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating poem");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class="generating">⌛ Generating a recipe about ${instructionsInput.value}</div>`;
+
   axios.get(apiUrl).then(displayRecipe);
 }
 
